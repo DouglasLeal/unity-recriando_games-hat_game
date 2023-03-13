f2fab511ddae80e370cd16c3ad8999d208e8f6a6
Douglas Leal <douglas.leal@live.com>
2023-03-12 21:58:53 -0300

    pontuando

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private GameController gameController;
+
+    private void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
     void Update()
     {
         if(transform.position.y < -11)
         {
-            Destroy(gameObject);
+            DestruirObjeto();
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Point"))
+        {
+            gameController.Pontuar();
+            DestruirObjeto();
+        }
+    }
+
+    private void DestruirObjeto()
+    {
+        Destroy(gameObject);
     }
 }
